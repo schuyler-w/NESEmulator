135bde34937f49cea392803cdd60788c130f3171
Schuyler Wang <schuyler.rw@gmail.com>
2024-06-04 22:59:46 -0700

    implemented joystick header, included SDL2 in makefile

---
+++ core/Joystick.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <SDL.h>
+#include <cstdio>
+
+#include <string>
+
+#include "typedefs.hpp"
+#include "BusInterface.hpp"
---
+++ core/Mapper/Mapper.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+#include "../typedefs.hpp"
+
+namespace NES {
+
+class Mapper {
+public:
+
+protected:
+
+};
+
+} // end namespace NES
