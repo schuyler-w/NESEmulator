5efdd01a2dc0e5449ab2d4688db1013dea15ae01
Schuyler Wang <schuyler.rw@gmail.com>
2024-06-05 00:12:31 -0700

    Implementations of main

---
+++ src/Desktop/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include <SDL.h>
+#include <iostream>
+#include <map>
+
+#include "../core/CPU.hpp"
+#include "../core/Mapper/Mapper.hpp"
+#include "../core/PPU.hpp"
+#include "../core/ROM.hpp"
+#include "../core/Joystick.hpp"
+
+int main(int argc, char **argv) {
+
+
+    return 0;
+}
