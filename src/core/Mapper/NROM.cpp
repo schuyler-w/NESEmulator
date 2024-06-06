1c961da5c9814a099bf6ed1e965d63b484ccf8aa
Schuyler Wang <schuyler.rw@gmail.com>
2024-06-05 20:05:52 -0700

    Implemented first mapper: NROM

---
+++ src/core/Mapper/NROM.cpp
@@ -0,0 +1,18 @@
+#include "NROM.hpp"
+
+namespace NES {
+
+    u8 NROM::read(u16 address) {
+        if (address < 0x8000) {
+            return 0;
+        }
+
+        address = (address - 0x8000) % prgCode.size();
+        return prgCode[address];
+    }
+
+    void NROM::write(u16 address, u8 data) {
+        //No write in NROM
+    }
+
+}  //namespace MedNES
---
+++ src/core/Mapper/NROM.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Mapper.hpp"
+
+namespace NES {
+
+    class NROM : public Mapper {
+    public:
+        NROM(std::vector<u8> &prgCode, std::vector<u8> &chrROM, int mirroring) : Mapper(prgCode, chrROM, mirroring) {}
+        ~NROM() override = default;
+        u8 read(u16 address) override;
+        void write(u16 address, u8 data) override;
+    };
+
+};  //namespace MedNES
