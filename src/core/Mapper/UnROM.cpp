f643a6de13098f8c29c44b50adbfa3a46ff8628f
Schuyler Wang <schuyler.rw@gmail.com>
2024-06-05 20:32:39 -0700

    implemented UnROM

---
+++ src/core/Mapper/UnROM.cpp
@@ -0,0 +1,29 @@
+#include "UnROM.hpp"
+
+namespace NES {
+
+    u8 UnROM::read(u16 address) {
+        if (address < 0x8000) {
+            return 0;
+        }
+
+        u32 address_32 = 0;
+
+        if (address >= 0xC000) {
+            address_32 = lastBankStart + (address - 0xC000);
+        } else {
+            address_32 = (address - 0x8000) + bankSelect * 16384;
+        }
+
+        return prgCode[address_32];
+    }
+
+    void UnROM::write(u16 address, u8 data) {
+        if (address < 0x8000) {
+            return;
+        }
+
+        bankSelect = data & 7;
+    }
+
+}  //namespace NES
---
+++ src/core/Mapper/UnROM.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Mapper.hpp"
+
+namespace NES {
+
+    class UnROM : public Mapper {
+    public:
+        UnROM(std::vector<u8> &prgCode, std::vector<u8> &chrROM, int mirroring) : Mapper(prgCode, chrROM, mirroring) {
+            lastBankStart = prgCode.size() - 16384;
+        }
+        ~UnROM() override = default;
+        u8 read(u16 address) override;
+        void write(u16 address, u8 data) override;
+
+    private:
+        u8 bankSelect = 0;
+        u32 lastBankStart = 0;
+    };
+
+};  //namespace NES
